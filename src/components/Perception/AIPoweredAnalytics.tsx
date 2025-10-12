0f1f322cb9cd8b1b49f95ae13815921d81f32018
rishicds <rishi.404@outlook.com>
2025-10-12 16:52:50 +0530

    Update AIPoweredAnalytics component to include heading for improved clarity

--- src/components/Perception/AIPoweredAnalytics.tsx
+++ src/components/Perception/AIPoweredAnalytics.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 const AIPoweredAnalytics = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full py-8">
+      <h1 className='uppercase text-4xl font-bold mb-4'>AI Powered Analytics Comes to life</h1>
       <Image
         src="/perception/aipoweredanalytics.png"
         alt="AI Powered Advanced Analytics Flow"
