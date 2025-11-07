f970792dda20025732f2688d087c43dfaf7e09f9
rishicds <rishi.404@outlook.com>
2025-11-07 22:46:21 +0530

    feat: Add custom headers configuration for cache control

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,16 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
